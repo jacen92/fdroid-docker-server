74795b81b08d4659eb44b9ccd572665d2cbee1cf
Jacen <jacen92@gmail.com>
2019-02-02 14:55:03 +0100

    use rssh ssh server to transfert applications to this node

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 Docker image with fdroid server for x64_86 and arm32v7
 
-This project contains a fdroid repository to deploy Android applications based on (https://guardianproject.info/2013/11/05/setting-up-your-own-app-store-with-f-droid/).
-with this repository you will be able to build and deploy your own fdroid server.
-
+This project contains an fdroid repository to deploy Android applications based on (https://guardianproject.info/2013/11/05/setting-up-your-own-app-store-with-f-droid/).
+With this repository you will be able to build and deploy your own headless fdroid server.
+This fdroid server is exposed in the container port 80.
 
 How to build it:
+
 ```
 cd docker
 docker build --rm -t fdroid -f Dockerfile .
@@ -13,9 +14,30 @@ docker build --rm -t fdroid -f Dockerfile.arm .
 
 ```
 
-How to use it:
+Customize config:
+-----------------
+
+You can customize the configuration file by providing a `config.in.py` file copy in `/opt/config`.
+
+
+Use with volume:
+----------------
+
+You can use this image to store your apk in a volume with `-v /your/persistant/dir:/opt/apk`.
+In this way you just have to cp your file in the host directory and the watcher will handle it.
+
+
+Use with scp:
+-------------
+
+Most of us will use this with a CI server to build and upload apk. To do that this image contain an ssh deamon limited to scp.
+Note that in this case the volume `/opt/apk` will not be watched for new files and the only way to put apk to this container become with scp.
+The ssh deamon is exposed to port 22 and the user is fdroid.
+To get access you can use the copy the auth_template.txt file to `/opt/config/auth.txt` with an other password (fdroid:other_password).
+You also can provide an authorized_keys file in the same way in `/opt/config`.
+At startup the container will set up the ssh deamon then you should be able to scp your file with:
+
 ```
-cd docker
-docker run --rm -d --n fdroid -v $pwd:/opt/apk -p 8031:80 fdroid:latest
+scp myapp.apk fdroid@myserver:~/
 
 ```
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,41 +1,83 @@
 #!/bin/bash
 
-echo "Copy apks from $APK_EXTERNAL_PATH"
-cp "$APK_EXTERNAL_PATH/*.apk" "$HTML_INTERNAL_PATH/repo/" 2>/dev/null
+# Set the directory to watch
+export APK_DIR_TO_WATCH="$APK_EXTERNAL_PATH"
 
+# If an user auth file is provided then copy, reset configuration and the directory to watch
+if [ -f "$CONFIG_EXTERNAL_PATH/auth.txt" ]; then
+  echo "Update fdroid scp password"
+  chpasswd < "$CONFIG_EXTERNAL_PATH/auth.txt"
+  sed -i -E 's/(.*)PasswordAuthentication (.*)/PasswordAuthentication yes/' /etc/ssh/sshd_config
+  export APK_DIR_TO_WATCH="/home/fdroid"
+else
+  echo "No custom auth.txt file found in $CONFIG_EXTERNAL_PATH"
+  sed -i -E 's/(.*)PasswordAuthentication (.*)/PasswordAuthentication no/' /etc/ssh/sshd_config
+fi
+
+# If an ssh key file is provided then copy, reset configuration and the directory to watch
+if [ -f "$CONFIG_EXTERNAL_PATH/authorized_keys" ]; then
+  echo "Copy fdroid scp keys"
+  mkdir /home/fdroid/.ssh
+  cp "$CONFIG_EXTERNAL_PATH/authorized_keys" /home/fdroid/.ssh/authorized_keys
+  sed -i -E 's/(.*)PubkeyAuthentication (.*)/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+  export APK_DIR_TO_WATCH="/home/fdroid"
+else
+  echo "No custom authorized_keys file found in $CONFIG_EXTERNAL_PATH"
+fi
+
+# If an server config file is provided then copy else if config.in file is provided copy it
 if [ -f "$CONFIG_EXTERNAL_PATH/$CONFIG_PY" ]; then
   echo "Copy $CONFIG_PY from $CONFIG_EXTERNAL_PATH"
   cp "$CONFIG_EXTERNAL_PATH/$CONFIG_PY" "$HTML_INTERNAL_PATH/"
   chmod 0600 "$HTML_INTERNAL_PATH/$CONFIG_PY"
-  if [ -f "$CONFIG_EXTERNAL_PATH/keystore.jks" ]; then
-    echo "Copy keystore.jks from $CONFIG_EXTERNAL_PATH"
-    cp "$CONFIG_EXTERNAL_PATH/keystore.jks" "$HTML_INTERNAL_PATH"
-  else
-    echo "No custom keystore.jks file found"
-  fi
 else
   echo "No custom $CONFIG_PY file found"
   if [ -f "$CONFIG_EXTERNAL_PATH/$CONFIG_IN_PY" ]; then
     echo "Copy $CONFIG_IN_PY from $CONFIG_EXTERNAL_PATH"
     cp "$CONFIG_EXTERNAL_PATH/$CONFIG_IN_PY" "$HTML_INTERNAL_PATH/"
+    python "$HTML_INTERNAL_PATH/$CONFIG_IN_PY"
   else
-    echo "No custom $CONFIG_IN_PY file found"
+    echo "No custom $CONFIG_IN_PY file found in $CONFIG_EXTERNAL_PATH"
   fi
-  python "$HTML_INTERNAL_PATH/$CONFIG_IN_PY"
 fi
 
+# If a custom keystore file is provided then copy it
+if [ -f "$CONFIG_EXTERNAL_PATH/keystore.jks" ]; then
+  echo "Copy keystore.jks from $CONFIG_EXTERNAL_PATH"
+  cp "$CONFIG_EXTERNAL_PATH/keystore.jks" "$HTML_INTERNAL_PATH"
+else
+  echo "No custom keystore.jks file found in $CONFIG_EXTERNAL_PATH"
+fi
+
+# Copy persistant apk files
+echo "Copy apks from $APK_EXTERNAL_PATH"
+find "$APK_EXTERNAL_PATH" -name '*.apk' -exec cp '{}' "$HTML_INTERNAL_PATH/repo/" \;
+
+# Load fdroid server metadata from existing apk
 echo "Run fdroid update"
 cd "$HTML_INTERNAL_PATH" && fdroid update -c && fdroid update && cd -
 
-echo "Starting nginx daemon"
-nginx -g 'daemon off;' &
+echo "Starting nginx and ssh daemons"
+nginx -g 'daemon off;' & /usr/sbin/sshd
 
 # TODO: find smarter way to achieve that...
-echo "Monitoring apk directory"
-inotifywait -m "$APK_EXTERNAL_PATH" -e create -e moved_to |
+echo "Monitoring $APK_DIR_TO_WATCH directory"
+inotifywait -m "$APK_DIR_TO_WATCH" -e create -e moved_to |
     while read path action file; do
         echo "The file '$file' appeared in directory '$path' via '$action'"
-        sleep 10
-        cp "$path$file" "$HTML_INTERNAL_PATH/repo/$file"
-        cd "$HTML_INTERNAL_PATH" && fdroid update -c && fdroid update && cd -
+        if [ "${file: -4}" == ".apk" ]; then
+          sleep 10
+          cp "$path$file" "$HTML_INTERNAL_PATH/repo/$file"
+          cd "$HTML_INTERNAL_PATH" && fdroid update -c && fdroid update && cd -
+          # Backup in persistant directory if using scp
+          if [ $APK_DIR_TO_WATCH == "/home/fdroid" ]; then
+            echo "Backup to persistant volume"
+            cp "$path$file" "$APK_EXTERNAL_PATH/$file"
+          fi
+        elif [[ "${file: -4}" == ".zip" ]]; then
+          echo "Not implemented for $file"
+        else
+          echo "Unknow file $file ... removed"
+          rm -rf "$path$file"
+        fi
     done
